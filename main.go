55ca3632f29ea4d20e42a1733a14c1ebb604b8a5
V-Mokhun <mohunmax1408@gmail.com>
2024-04-20 15:21:59 +0200

    Chapters 2.1 through 2.4. Basic mux and http operations

--- main.go
+++ main.go
@@ -6,16 +6,39 @@ import (
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
 	w.Write([]byte("Hello"))
+}
+
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Viewing a snippet.."))
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", http.MethodPost)
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	w.Write([]byte("Creating a snippet.."))
 }
 
 func main() {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
+	// new way to handle different methods, introduced in Go 1.22:
+	// mux.HandleFunc("POST /snippet/create", snippetCreate)
 
 	log.Print("Starting server on :4000")
-	err := http.ListenAndServe(":4000", mux)
+	err := http.ListenAndServe("localhost:4000", mux)
 	if err != nil {
 		log.Fatal(err)
 	}
